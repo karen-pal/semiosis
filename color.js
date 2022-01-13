ccf6f95b2f338d3fc21a92fef93b46f6601351db
Karen Palacio <karen.palacio.1994@gmail.com>
2022-01-12 21:53:29 -0300

    empezar a parsear el color ingresado para dibujar en la consola

--- color.js
+++ color.js
@@ -1,8 +1,11 @@
 const nearley = require("nearley");
 const grammar = require("./csscolor.js");
 
+//cli
 const readlineSync = require('readline-sync');
+const  chalk  = require('chalk');
 
+console.log(chalk)
 let parser; 
 
 // Read color from console
@@ -10,9 +13,19 @@ while (true) {
     const color = readlineSync.question('Please write a css color: \n');
     parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar),{ keepHistory: true });
     try {
-        parser.feed(color)
-        console.log(parser.results);
+        parser.feed(color);
+        let colorify;
+        if (parser.results[0][0] === "#") {
+            colorify = chalk.hex(color);
+        } else {
+            col = parser.results[0]
+            console.log(col)
+            colorify = chalk.rgb(255,255,255)
+        }
+        console.log(colorify(parser.results));
+        console.log();
     } catch(parseError) {
+        console.log(parseError);
         console.log("{!!!} Error at character " + parseError.offset)
     }
 }
