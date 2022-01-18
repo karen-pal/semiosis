9ceca139efecda4070cb2cb61fcf57fc0723605d
Karen Palacio <karen.palacio.1994@gmail.com>
2022-01-18 17:17:17 -0300

    empezar a agarrarle la onda a este ejemplo

--- colorNatural/colNat.js
+++ colorNatural/colNat.js
@@ -2,7 +2,25 @@
 // http://github.com/Hardmath123/nearley
 (function () {
 function id(x) { return x[0]; }
- function LENGTH (d) { return d[0][0].length; } var grammar = {
+ function VERDEFUN (d) { 
+                return {
+                    d: d,
+                    hue: 120,
+                    saturation: 100,
+                    lightness: 50
+                }; } 
+ function ROJOFUN (d) { 
+                return {
+                    d: d,
+                    hue: 0,
+                    saturation: 100,
+                    lightness: 50
+                }; } 
+ function TEST (d) { 
+                return {
+                    d: d,
+                    v: d[0][0].hue
+                }; } var grammar = {
     Lexer: undefined,
     ParserRules: [
     {"name": "_$ebnf$1", "symbols": []},
@@ -12,7 +30,7 @@ function id(x) { return x[0]; }
     {"name": "__$ebnf$1", "symbols": ["__$ebnf$1", "wschar"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "__", "symbols": ["__$ebnf$1"], "postprocess": function(d) {return null;}},
     {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id},
-    {"name": "main", "symbols": ["Hue"], "postprocess": LENGTH},
+    {"name": "main", "symbols": ["Hue"], "postprocess": TEST},
     {"name": "Saturacion$string$1", "symbols": [{"literal":"f"}, {"literal":"u"}, {"literal":"e"}, {"literal":"r"}, {"literal":"t"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "Saturacion", "symbols": ["Luz", "_", "Saturacion$string$1"]},
     {"name": "Saturacion$string$2", "symbols": [{"literal":"a"}, {"literal":"p"}, {"literal":"a"}, {"literal":"g"}, {"literal":"a"}, {"literal":"d"}, {"literal":"o"}], "postprocess": function joiner(d) {return d.join('');}},
@@ -22,18 +40,13 @@ function id(x) { return x[0]; }
     {"name": "Luz", "symbols": ["Hue", "_", "Luz$string$1"]},
     {"name": "Luz$string$2", "symbols": [{"literal":"o"}, {"literal":"s"}, {"literal":"c"}, {"literal":"u"}, {"literal":"r"}, {"literal":"o"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "Luz", "symbols": ["Hue", "_", "Luz$string$2"]},
-    {"name": "Luz", "symbols": ["Hue"], "postprocess": 
-        
-        function([a, b , data]) {
-            return(a)
-            }
-        },
-    {"name": "Hue$string$1", "symbols": [{"literal":"r"}, {"literal":"o"}, {"literal":"j"}, {"literal":"o"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Hue", "symbols": ["Hue$string$1"]},
-    {"name": "Hue$string$2", "symbols": [{"literal":"a"}, {"literal":"z"}, {"literal":"u"}, {"literal":"l"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Hue", "symbols": ["Hue$string$2"]},
-    {"name": "Hue$string$3", "symbols": [{"literal":"v"}, {"literal":"e"}, {"literal":"r"}, {"literal":"d"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "Hue", "symbols": ["Hue$string$3"]}
+    {"name": "Luz", "symbols": ["Hue"]},
+    {"name": "Hue", "symbols": ["ROJO"]},
+    {"name": "Hue", "symbols": ["VERDE"]},
+    {"name": "ROJO$string$1", "symbols": [{"literal":"r"}, {"literal":"o"}, {"literal":"j"}, {"literal":"o"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "ROJO", "symbols": ["ROJO$string$1"], "postprocess": ROJOFUN},
+    {"name": "VERDE$string$1", "symbols": [{"literal":"v"}, {"literal":"e"}, {"literal":"r"}, {"literal":"d"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "VERDE", "symbols": ["VERDE$string$1"], "postprocess": VERDEFUN}
 ]
   , ParserStart: "main"
 }
--- colorNatural/colNat.ne
+++ colorNatural/colNat.ne
@@ -1,13 +1,33 @@
 
 @builtin "whitespace.ne" # `_` means arbitrary amount of whitespace
 
-@{% function LENGTH (d) { return d[0][0].length; } %}
-main -> Hue {% LENGTH %}
-Saturacion -> Luz _ "fuerte" | Luz _ "apagado" | Luz 
-Luz -> Hue _ "claro" | Hue _ "oscuro" | Hue {%
+@{% function VERDEFUN (d) { 
+                return {
+                    d: d,
+                    hue: 120,
+                    saturation: 100,
+                    lightness: 50
+                }; } %}
 
-    function([a, b , data]) {
-        return(a)
-        }
-%}
-Hue -> "rojo" | "azul" | "verde"
+@{% function ROJOFUN (d) { 
+                return {
+                    d: d,
+                    hue: 0,
+                    saturation: 100,
+                    lightness: 50
+                }; } %}
+
+@{% function TEST (d) { 
+                return {
+                    d: d,
+                    v: d[0][0].hue
+                }; } %}
+main -> Hue {% TEST %}
+
+Saturacion -> Luz _ "fuerte" | Luz _ "apagado" | Luz 
+Luz -> Hue _ "claro" | Hue _ "oscuro" | Hue
+Hue -> ROJO | VERDE 
+
+ROJO -> "rojo" {% ROJOFUN %}
+
+VERDE -> "verde" {% VERDEFUN %}
